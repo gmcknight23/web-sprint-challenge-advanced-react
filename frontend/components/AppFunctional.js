ffc7e9ba0e69170bd5ab197360502f3d0876d6bf
gregmcknight <gmcknight23@gmail.com>
2022-03-11 19:58:21 -0500

    class and function work with 91 pass

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,158 +1,3 @@
-// import React, { useState } from "react";
-
-// export default function AppFunctional(props) {
-//   const [x, setX] = useState(2);
-//   const [y, setY] = useState(2);
-//   const [email, setEmail] = useState("");
-//   const [steps, setSteps] = useState(0);
-//   const [message, setMessage] = useState("");
-//   const URL = "http://localhost:9000/api/result";
-
-//   const moveRight = () => {
-//     if (x < 3) {
-//       setX(x + 1);
-//       setSteps(steps + 1);
-//       setMessage("");
-//     } else {
-//       setMessage("You can't go right");
-//     }
-
-//   }
-//     //   setState({
-//     //     x: this.state.x + 1,
-//     //     steps: this.state.steps + 1,
-//     //     message: "",
-//     //   });
-//     // } else {
-//     //   this.setState({ message: "You can't go right" });
-//     // }
-//     //  };
-//     const moveLeft = () => {
-//       if (x > 1) {
-//         setX(x - 1);
-//         setSteps(steps + 1);
-//         setMessage("");
-//       } else {
-//         setMessage("You can't go left");
-//       }
-//     };
-//     const moveUp = () => {
-//       if (y > 1) {
-//         setX(y - 1);
-//         setSteps(steps + 1);
-//         setMessage("");
-//       } else {
-//         setMessage("You can't go up");
-//       }
-//     };
-//     const moveDown = () => {
-//       if (y < 3) {
-//         setX(y + 1);
-//         setSteps(steps + 1);
-//         setMessage("");
-//       } else {
-//         setMessage("You can't go down");
-//       }
-//     };
-//     reset = () => {
-//       setX(2);
-//       setY(2);
-//       setSteps(0);
-//       setEmail("");
-//       setMessage("");
-//     };
-//     handleChange = (e) => {
-//       setEmail(e.target.value);
-//     };
-//     handleSubmit = (e) => {
-//       e.preventDefault();
-//       axios
-//         .post(URL, {
-//           x: x,
-//           y: y,
-//           email: email,
-//           steps: steps,
-//         })
-//         .then((res) => {
-//           setMessage(res.data.message);
-//         })
-//         .catch((err) => {
-//           setMessage(err.response.data.message);
-//         });
-//     };
-
-//     return (
-//       <div id="wrapper" className={props.className}>
-//         <div className="info">
-//           <h3 id="coordinates">
-//             Coordinates ({x}, {y})
-//           </h3>
-//           <h3 id="steps">You moved {steps} times</h3>
-//         </div>
-//         <div id="grid">
-// <div className={x === 1 && y === 1 ? "square active" : "square"}>
-//   {x === 1 && y === 1 ? "B" : ""}
-// </div>
-// <div className={x === 2 && y === 1 ? "square active" : "square"}>
-//   {x === 2 && y === 1 ? "B" : ""}
-// </div>
-// <div className={x === 3 && y === 1 ? "square active" : "square"}>
-//   {x === 3 && y === 1 ? "B" : ""}
-// </div>
-// <div className={x === 1 && y === 2 ? "square active" : "square"}>
-//   {x === 1 && y === 2 ? "B" : ""}
-// </div>
-// <div className={x === 2 && y === 2 ? "square active" : "square"}>
-//   {x === 2 && y === 2 ? "B" : ""}
-// </div>
-// <div className={x === 3 && y === 2 ? "square active" : "square"}>
-//   {x === 3 && y === 2 ? "B" : ""}
-// </div>
-// <div className={x === 1 && y === 3 ? "square active" : "square"}>
-//   {x === 1 && y === 3 ? "B" : ""}
-// </div>
-// <div className={x === 2 && y === 3 ? "square active" : "square"}>
-//   {x === 2 && y === 3 ? "B" : ""}
-// </div>
-// <div className={x === 3 && y === 3 ? "square active" : "square"}>
-//   {x === 3 && y === 3 ? "B" : ""}
-// </div>
-//         </div>
-//         <div className="info">
-//           <h3 id="message">{message}</h3>
-//         </div>
-//         <div id="keypad">
-//           <button id="left" onClick={moveLeft}>
-//             LEFT
-//           </button>
-//           <button id="up" onClick={moveUp}>
-//             UP
-//           </button>
-//           <button id="right" onClick={moveRight}>
-//             RIGHT
-//           </button>
-//           <button id="down" onClick={moveDown}>
-//             DOWN
-//           </button>
-//           <button id="reset" onClick={reset}>
-//             reset
-//           </button>
-//         </div>
-//         <form onSubmit={handleSubmit}>
-//           <input
-//             id="email"
-//             type="email"
-//             placeholder="type email"
-//             value={email}
-//             onChange={(e) => setEmail(e.target.value)}
-//           ></input>
-//           <input id="submit" type="submit"></input>
-//         </form>
-//       </div>
-//     );
-//   };
-// }
-
 import React, { useState } from "react";
 
 export default function AppFunctional(props) {
@@ -209,6 +54,27 @@ export default function AppFunctional(props) {
     setMessage("");
   };
 
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(URL, {
+        x: x,
+        y: y,
+        email: email,
+        steps: steps,
+      })
+      .then((res) => {
+        setMessage(res.data.message);
+      })
+      .catch((err) => {
+        setMessage(err.response.data.message);
+      });
+  };
+
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
@@ -247,7 +113,7 @@ export default function AppFunctional(props) {
         </div>
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
         <button id="left" onClick={moveLeft}>
@@ -266,8 +132,14 @@ export default function AppFunctional(props) {
           reset
         </button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="type email"></input>
+      <form onSubmit={handleSubmit}>
+        <input
+          id="email"
+          type="email"
+          placeholder="type email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
